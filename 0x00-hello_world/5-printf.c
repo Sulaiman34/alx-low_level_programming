1c099c21822cf932c42c7041697e6f447dfba34e
sulaiman34 <sulaiman.folorunsho90@.gmail.com>
2023-03-20 06:19:02 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,8 @@
  * Return: 0
  */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}trim trailing whitespace
+}	
+
